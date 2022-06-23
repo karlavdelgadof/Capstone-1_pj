0c0eca31a88e4d882f212eb3f37880ad04a40486
karlavdelgadof <karlavdelgadof@gmail.com>
2022-06-23 14:12:02 -0500

    Correct img paths

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 const featuredArtists = [
   {
     name: 'Machine Gun Kelly',
-    picture: './multimedia/mgk.png',
+    picture: '../multimedia/mgk.png',
     description: 'Delivering lyrics with the speed of a bullet train, Cleveland rapper Machine Gun Kelly experienced a meteoric rise in late 2011.',
     genre: 'Rap, Pop/rock, Punk',
   },
   {
     name: 'Green Day',
-    picture: './multimedia/greenday.png',
+    picture: '../multimedia/greenday.png',
     description: 'One of the most celebrated and successful bands in the world. Snarling, energetic, and highly melodic brand of pop-punk.',
     genre: 'Pop/rock, Punk',
   },
   {
     name: 'Bruce Springsteen',
-    picture: './multimedia/bruce.png',
+    picture: '../multimedia/bruce.png',
     description: 'American singer, songwriter, and bandleader who became the archetypal rock performer of the 1970s and \'80s.',
     genre: 'Heartland rock, Americana',
   },
   {
     name: 'Taylor Swift',
-    picture: './multimedia/tay.png',
+    picture: '../multimedia/tay.png',
     description: 'Multi-Grammy award-winning American singer-songwriter who became the youngest artist in history to win the Grammy Award for Album of the Year.',
     genre: 'Pop, Country, Indie',
   },
   {
     name: 'Martin Garrix',
-    picture: './multimedia/martin.png',
+    picture: '../multimedia/martin.png',
     description: 'Topping DJ rank lists in the late 2010s, Garrix has remained a global club favorite, climbing the charts with high-profile crossover collaborations. ',
     genre: 'Electronic, House, EDM',
   },
   {
     name: 'Lasso',
-    picture: './multimedia/lasso.png',
+    picture: '../multimedia/lasso.png',
     description: 'Latin Grammy nominee for Best New Artist 2021.Winner Pop Rock Artist of the Year at Premios Heat 2015.',
     genre: 'Pop-rock, flamanco',
   },
